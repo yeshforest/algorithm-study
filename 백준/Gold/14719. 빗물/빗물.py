66d4b62152866642e067639320f103e247e179d5
HaeRimRoh <mtxpvksh@gmail.com>
2025-04-18 13:38:20 +0900

    [Gold V] Title: 빗물, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -0,0 +1,12 @@
+y, x = map(int,input().split())
+block = list(map(int,input().split()))
+answer = 0
+ 
+for i in range(1, x-1):
+    left = max(block[ :i])
+    right = max(block[i+1: ])
+    m = min(left, right)
+    if m > block[i]:
+        answer += m - block[i]
+ 
+print(answer)
