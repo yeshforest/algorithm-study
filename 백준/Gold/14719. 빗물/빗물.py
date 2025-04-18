f4ccb7a22443a4b909d3babfbeae62e257236b61
HaeRimRoh <mtxpvksh@gmail.com>
2025-04-18 13:40:38 +0900

    [Gold V] Title: 빗물, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.py
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -2,11 +2,11 @@ y, x = map(int,input().split())
 block = list(map(int,input().split()))
 answer = 0
  
-for i in range(1, x-1):
-    left = max(block[ :i])
-    right = max(block[i+1: ])
-    m = min(left, right)
-    if m > block[i]:
-        answer += m - block[i]
+for i in range(1, x-1): # 처음, 마지막은 제외하고
+    left = max(block[ :i]) # 본인 기준 최대값의 왼쪽
+    right = max(block[i+1: ]) # 본인 기준 최대값의 오른쪽
+    m = min(left, right)  # 그중 최솟값만큼
+    if m > block[i]: # 현재블록보다 높으면
+        answer += m - block[i] # 누적합
  
 print(answer)
