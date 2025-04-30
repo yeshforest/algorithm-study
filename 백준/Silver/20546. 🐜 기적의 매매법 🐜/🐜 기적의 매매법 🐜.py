b9106336cca8cda3f72e4b19fc6523bb0ce30574
HaeRimRoh <mtxpvksh@gmail.com>
2025-04-30 22:41:04 +0900

    [Silver V] Title: 🐜 기적의 매매법 🐜, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/20546. 🐜 기적의 매매법 🐜/🐜 기적의 매매법 🐜.py
+++ 백준/Silver/20546. 🐜 기적의 매매법 🐜/🐜 기적의 매매법 🐜.py
@@ -1,50 +1,41 @@
-money = int(input())
-price = list(map(int,input().split()))
+# 준현이 VS 성민이
 
-jMoney = money # 현금
+Money = int(input())                                # 초기 현금
+MachineDuck = list(map(int, input().split()))       # 1일부터 14일까지의 주가
+
+# 준현이
+jMoney = Money    # 현금 
 jStoke = 0 # 주 수
-sMoney = money # 현금
-sStoke = 0 # 주 수
-# 준현: junhun에서 최댓값만큼 삼
-# price 끝날때까지 반복해서 결과값내기
 
-for i in range(len(price)):
-  buy = 0
-  while jMoney - price[i] * (jStoke + 1) >= 0:
-    jStoke += 1
-    buy = price[i] * jStoke # 가격 * 재고
-  jMoney = jMoney - buy
+# 성민이
+sMoney = Money
+sStoke = 0
 
-jCurrentMoney = (price[13] * jStoke) + jMoney
+for idx, price in enumerate(MachineDuck): # 인덱스와 값을 동시에 접근
 
-# 성민: sMoney에서 
-# 3일 연속상승 : 다음 날 전량매도, 3일연속하락 : 다음 날 전량매수
+    # 준현
+    jStoke += (jMoney // price)
+    jMoney %= price
 
-# 3일 전까지 반복
-# 마지막날 계산
+    # 성민
+    temp = MachineDuck[idx:idx+4] # 현재 index ~ index+3
 
-for i in range(3,len(price)):
-  buy = 0
-  tempStoke=0
-  # 3일연속 하락, 전량매수
-  if (price[i-1] - price[i-2] < 0) and (price [i-2] - price[i-3] < 0):
-    while sMoney - price[i] * (tempStoke+1) >= 0 : # 돈이 없을때까지
-      tempStoke +=1
-      buy = price[i] * tempStoke
-    sMoney = sMoney - buy
-    sStoke += tempStoke
-  # 3일연속 상승, 전량매도
-  if price[i-1] - price[i-2] > 0 and price [i-2] - price[i-3] > 0:
-    if sStoke > 0:
-      sMoney += price[i]* sStoke
-      sStoke=0
+    if len(temp) < 4: # 3개 이하이면 범위 벗어나므로 넘어가기
+        continue
 
-sCurrentMoney = (price[13] * sStoke) + sMoney
+    if temp[0] < temp[1] < temp[2] < temp[3] and sStoke > 0:         # 매도
+        sMoney += (sStoke * temp[3])
+        sStoke = 0
+    elif temp[0] > temp[1] > temp[2] > temp[3]:                             # 매수
+        sStoke += (sMoney // temp[3])
+        sMoney %= temp[3]
 
-if jCurrentMoney == sCurrentMoney:
-  print("SAMESAME")
-elif jCurrentMoney > sCurrentMoney:
-  print("BNP")
-elif (jCurrentMoney < sCurrentMoney):
-  print("TIMING")
+answer_bnp = jMoney + jStoke * MachineDuck[-1] # 맨뒤의 값..
+answer_timing = sMoney + sStoke * MachineDuck[-1] 
 
+if answer_bnp < answer_timing:
+    print("TIMING")
+elif answer_bnp > answer_timing:
+    print("BNP")
+else:
+    print("SAMESAME")
