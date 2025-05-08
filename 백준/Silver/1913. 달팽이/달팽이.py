0fcb4c16797f9217d3ee525f8e53188f38960549
HaeRimRoh <mtxpvksh@gmail.com>
2025-05-08 16:43:36 +0900

    [Silver III] Title: 달팽이, Time: 784 ms, Memory: 70928 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.py
+++ 백준/Silver/1913. 달팽이/달팽이.py
@@ -1,86 +1,34 @@
-# n은 홀수
-# 1부터 n2까지의 자연수를 달팽이 모양으로..
-
 n = int(input())
-target=int(input())
-number = n**2
+num = int(input())
+dx = [-1,0,1,0]
+dy = [0,1,0,-1]  # 상, 우, 하, 좌 순서
+graph = [[0]*n for _ in range(n)]
+number = 2
+x = n//2
+y = n//2
+graph[x][y] = 1 # 배열의 중앙부터 시작
+repeat = 1 # 한 방향으로 몇 칸 갈지 결정
+i = 0 # dx,dy 가리키는 변수
 
-arr = [[0 for _ in range (n)] for _ in range (n)]
-x=0
-y=0
+answer = [x+1,y+1]
+while x!=0 or y !=0:
+   flag = 0
+   for _ in range(2):
+      for _ in range(repeat): # 같은 반복 횟수로 두 방향(예: 위-오른쪽)을 이동
+         x += dx[i]
+         y += dy[i]
+         graph[x][y] = number
+         if number == num:  # 위치를 찾고자 하는 자연수이면 answer에 위치 저장
+            answer = [x+1,y+1] 
+         if x == 0 and y == 0: 
+            flag = 1 # 좌측 상단(0,0)에 도달했으면 종료 준비
+            break 
+         number += 1
+      if flag == 1: break
+      i = (i+1)%4 # 다음 방향으로 전환
+   repeat += 1  # 이동 거리 증가
 
-# 맨 첫 수 저장
-arr[x][y] = number
-# 초기 아래 -> 오 -> 위 저장
-for i in range(n-1): # n-1번 아래로
-  x +=1 
-  y +=0
-  number -= 1
-  arr[x][y] = number
-
-for i in range(n-1): # n-1번 옆으로
-  x+=0 
-  y +=1
-  number-=1
-  arr[x][y] = number
-
-for i in range(n-1): # n-1번 위쪽으로
-  x -=1
-  y +=0
-  number-=1
-  arr[x][y] = number
-  
-
-# 왼,아,오,위 반복, 아래 과정을 n-2번 반복
-for i in range(n-2):
-  if i % 2 == 0: # 짝수번째: 왼, 아 
-    for j in range(n-2-i):
-      x+=0 
-      y-=1 #왼
-      number-=1
-      arr[x][y] = number
-    for j in range(n-2-i):
-      x+=1 
-      y +=0 #아
-      number-=1
-      arr[x][y] = number
-
-    
-  else: # 홀수 : 오,위
-    for j in range(n-2-i):
-      x+=0 
-      y +=1 #오
-      number-=1
-      arr[x][y] = number
-    for j in range(n-2-i):
-      x-=1 
-      y-=0 #위
-      number-=1
-      arr[x][y] = number
-    
-
-for i in range(len(arr)):
-  for j in range(len(arr)):
-    print(arr[i][j],end=" ")
-  print("")
-
-for i in range(len(arr)):
-  for j in range(len(arr)):
-    if arr[i][j] == target:
-      print(i+1,j+1)
-
-
-
-# 2번
-  # n-2번 왼쪽으로
-  # n-2번 아래로
-
-# 2번
-  # n-3번 위로
-  # n-3번 오른쪽으로
-
-# 2번
-  # n-4번 왼쪽으로
-  # n-4번 아래로
-
-
+for i in graph:
+   print(*i)
+   
+print(*answer)
