3cbe8e4ebf9404260326e79410e390eb7f8e84c5
HaeRimRoh <mtxpvksh@gmail.com>
2025-05-21 23:31:59 +0900

    [Silver III] Title: 수열, Time: 72 ms, Memory: 40120 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -1,24 +1,19 @@
-# 온도를 측정한 전체 날짜의 수
-# 합을 구하기 위한 연속적인 날짜의 수
+# 슬라이딩 윈도우로 풀어보자
 
-# 온도의 합이 최대가 되는 값을 출력
+N,K = map(int,input().split())
+days = list(map(int,input().split()))
 
-# 1 -6 -13 -9 3 10 20 21 5
-# 21
 
-n,k = map(int,input().split())
-arr = list(map(int,input().split()))
+sum_value = sum(days[:K]) # k전까지의 합
+max_value = sum_value # 최댓값의 초기값
 
-# 누적합 구하기
-sums = [0 for _ in range(n+1)] # 1~n까지 사용
 
-for i in range(n): # 0~n
-  sums[i+1] = arr[i]+sums[i] # sum[1] = 3 + 0, [0,3,1,-3,-12,-12,-9.-2,11,19,16]
+# 슬라이딩 윈도우
+for i in range(K,N):
+  sum_value -= days[i-K] # 왼쪽값은 빼주고
+  sum_value += days[i] # 오른쪽 값은 더해줌
+  if sum_value > max_value: # 최대값 갱신
+      max_value = sum_value
 
-# 최댓값 찾기
-answers=[]
 
-for i in range(k,n+1):
-  answers.append(sums[i]-sums[i-k]) # 기억 왜곡된걸 그대로 믿음..
-
-print(max(answers))
+print(max_value)
