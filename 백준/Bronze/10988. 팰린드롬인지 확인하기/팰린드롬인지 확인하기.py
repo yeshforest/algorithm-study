ec97ab836faf31d5c6413c9e92178eecbf49e652
HaeRimRoh <mtxpvksh@gmail.com>
2025-09-24 16:36:53 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
@@ -1,13 +1,8 @@
-# 첫번째, 마지막/ 두번쨰 마지막-1, 이렇게 비교해서 모두 같으면 o /2했을떄 소수이면 가운데 무시
 
 target = input()
-printONE = True
+reverseTarget = target[::-1]
 
-for i in range(len(target)):
-  if target[i] != target[len(target)-1-i]:
-    print(0)
-    printONE = False
-    break
-
-if printONE:
+if target == reverseTarget:
   print(1)
+else:
+  print(0)
